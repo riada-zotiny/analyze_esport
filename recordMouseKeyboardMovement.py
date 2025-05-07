1d46e1f5ad9c3c0c22fef5ec35a0c9e7b99ce461
riada-zotiny <amalya.mourih@gmail.com>
2025-05-07 10:28:04 +0200

    "Nouveau Commit avec le changement de codes et nouveaux enregistrement"

--- recordMouseKeyboardMovement.py
+++ recordMouseKeyboardMovement.py
@@ -1,9 +1,8 @@
-# https://github.com/biohacker0/maCrow/blob/main/requirements.txt
-
-
 import time
 import json
 import argparse
+import os
+from datetime import datetime
 from pynput import mouse as pynput_mouse
 import pyautogui
 import mouse
@@ -12,28 +11,24 @@ import ctypes
 import tkinter as tk
 from tkinter import Canvas, Label
 
-
-
 screen_width, screen_height = pyautogui.size()
 
 def set_cursor_pos(x, y):
     ctypes.windll.user32.SetCursorPos(x, y)
-    
+
 def countdown_animation(root):
     canvas = Canvas(root, width=screen_width, height=screen_height, bg="white", highlightthickness=0)
     canvas.pack()
-    countdown_label = Label(canvas, text="", font=("Arial", 16, "bold"), fg = "red")
+    countdown_label = Label(canvas, text="", font=("Arial", 16, "bold"), fg="red")
     countdown_label.place(relx=0.5, rely=0.5, anchor="center")
     if root.mode == "record":
         countdown_label.config(text="Enregistrement de Souris et de Clavier")
     elif root.mode == "replay":
-        countdown_label.config(text="Relancement des actions ")  
-
+        countdown_label.config(text="Relancement des actions")
     root.update()
     time.sleep(1)
     canvas.destroy()
-    
-    
+
 def count_down_animation_config(mode):
     print(f"{mode.capitalize()} will start in:")
     root = tk.Tk()
@@ -41,22 +36,33 @@ def count_down_animation_config(mode):
     root.overrideredirect(1)
     root.geometry(f"{screen_width}x{screen_height}+0+0")
     root.attributes('-topmost', 1)
-    root.mode = mode  # Set the mode attribute
+    root.mode = mode
     countdown_animation(root)
-    root.withdraw()  # Hide the root window
+    root.withdraw()
 
+def get_next_filename(base_dir="dataMouseKeybord", base_name="mouse_keyboard_actions"):
+    today = datetime.now().strftime("%Y-%m-%d")
+    i = 1
+    while True:
+        filename = f"{base_name}_{today}_num{i}.json"
+        full_path = os.path.join(base_dir, filename)
+        if not os.path.exists(full_path):
+            return filename
+        i += 1
 
-def record(filename):
+def record():
     count_down_animation_config("record")
     print("Recording started. Move the mouse around, perform actions, and type on the keyboard. Press Ctrl + C to stop.")
     actions = []
-    previous_time = time.time()  
+    previous_time = time.time()
+    time_diff_container = [0] 
+
     def on_move(x, y):
         if 0 <= x < screen_width and 0 <= y < screen_height:
             actions.append({
                 "action": "move",
                 "position": (x, y),
-                "time_diff": time_diff
+                "time_diff": time_diff_container[0]
             })
 
     def on_click(x, y, button, pressed):
@@ -65,7 +71,7 @@ def record(filename):
                 "action": "press" if pressed else "release",
                 "button": str(button),
                 "position": (x, y),
-                "time_diff": time_diff
+                "time_diff": time_diff_container[0]
             })
 
     def on_scroll(x, y, dx, dy):
@@ -74,7 +80,7 @@ def record(filename):
                 "action": "scroll",
                 "position": (x, y),
                 "scroll": dy,
-                "time_diff": time_diff
+                "time_diff": time_diff_container[0]
             })
 
     def on_key_event(event):
@@ -82,7 +88,7 @@ def record(filename):
             "action": "key",
             "key": event.name,
             "event_type": event.event_type,
-            "time_diff": time_diff
+            "time_diff": time_diff_container[0]
         })
 
     listener = pynput_mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
@@ -92,43 +98,40 @@ def record(filename):
     try:
         while True:
             current_time = time.time()
-            time_diff = current_time - previous_time
+            time_diff_container[0] = current_time - previous_time
             x, y = mouse.get_position()
             x = max(0, min(x, screen_width - 1))
             y = max(0, min(y, screen_height - 1))
 
             set_cursor_pos(x, y)
-            time.sleep(0.05)  
+            time.sleep(0.05)
 
     except KeyboardInterrupt:
         print("Recording stopped.")
         listener.stop()
         keyboard.unhook_all()
 
-        with open("dataMouseKeybord/" + filename, 'w') as file:
+        filename = get_next_filename()
+        path = os.path.join("dataMouseKeybord", filename)
+        with open(path, 'w') as file:
             json.dump(actions, file)
+        print(f"Actions saved to: {path}")
 
-
-# Function to replay mouse and keyboard actions
 def replay(filename, key_delay=0.1):
     count_down_animation_config("replay")
     with open("dataMouseKeybord/" + filename, 'r') as file:
         actions = json.load(file)
         print("Replaying mouse movements and keyboard inputs...")
 
-        # Variables for double click detection
         last_click_time = 0
-        double_click_threshold = 0.3  # Adjust this threshold as needed for your scenario
+        double_click_threshold = 0.3
 
-        for i in range(len(actions)):
-            action = actions[i]
+        for action in actions:
             if action["action"] == "move":
-                # Use mouse.move for smooth movement at normal speed
                 mouse.move(action["position"][0], action["position"][1], absolute=True, duration=0.00001)
+
             elif action["action"] == "press":
                 current_time = action["time_diff"]
-
-                # Check for double click
                 if current_time - last_click_time <= double_click_threshold:
                     if action["button"] == "Button.left":
                         pyautogui.mouseDown(button='left')
@@ -155,7 +158,6 @@ def replay(filename, key_delay=0.1):
                     print("normal right click")
 
             elif action["action"] == "scroll":
-                # Adjust the sleep time based on the duration of the scroll action
                 time.sleep(0.01)
                 mouse.wheel(delta=action["scroll"])
 
@@ -166,23 +168,20 @@ def replay(filename, key_delay=0.1):
                 elif action["event_type"] == "up":
                     keyboard.release(action["key"])
                     print(f"Key released: {action['key']}")
-
-                # Introduce a delay between key presses
                 time.sleep(key_delay)
 
         print("Replay complete.")
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Mouse and Keyboard Recorder/Replayer')
     parser.add_argument('command', choices=['record', 'replay'], help='Choose command: record or replay')
-    parser.add_argument('--file', default='mouse_keyboard_actions.json',
-                        help='File to save mouse and keyboard actions (default: mouse_keyboard_actions.json)')
+    parser.add_argument('--file', help='File to replay from (only for replay mode)')
     args = parser.parse_args()
 
     if args.command == 'record':
-        record(args.file)
+        record()
     elif args.command == 'replay':
-        replay(args.file)
-        
-        
+        if args.file:
+            replay(args.file)
+        else:
+            print("Please provide a --file argument for replay mode.")
