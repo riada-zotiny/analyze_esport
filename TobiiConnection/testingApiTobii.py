1ae69bce76ccea17db784c04687d6ca6691847e5
riada-zotiny <amalya.mourih@gmail.com>
2025-05-23 13:47:49 +0200

    COMMIT CLIENT

--- TobiiConnection/testingApiTobii.py
+++ TobiiConnection/testingApiTobii.py
@@ -1,8 +1,20 @@
 import asyncio
-from g3pylib import connect_to_glasses
+from g3pylib import (
+    DEFAULT_HTTP_PORT,
+    DEFAULT_RTSP_LIVE_PATH,
+    DEFAULT_RTSP_PORT,
+    connect_to_glasses,
+)
+from g3pylib.zeroconf import DEFAULT_WEBSOCKET_PATH, G3ServiceDiscovery
 
-G3_HOSTNAME = "192.168.75.51"
+G3_HOSTNAME = "TG03B-080202027181.local"
 
+async def test_connect_with_service_using_ip(g3_hostname: str):
+    g3_service = await G3ServiceDiscovery.request_service(g3_hostname)
+    async with connect_to_glasses.with_service(g3_service, using_ip=False) as g3:
+        serial = await g3.system.get_recording_unit_serial()
+        print(f"Connecté à : {serial} ")
+        
 async def connect():
     async with connect_to_glasses.with_hostname( G3_HOSTNAME, using_zeroconf=False, using_ip=True) as g3:
         serial = await g3.system.get_recording_unit_serial()
@@ -18,16 +30,16 @@ async def connect():
 
        
        
-async def main():
-    await connect()
+async def test_connect_with_urls(g3_hostname: str):
+    async with connect_to_glasses.with_url(
+        f"ws://{g3_hostname}{DEFAULT_WEBSOCKET_PATH}",
+        f"rtsp://{g3_hostname}:{DEFAULT_RTSP_PORT}{DEFAULT_RTSP_LIVE_PATH}",
+        f"http://{g3_hostname}:{DEFAULT_HTTP_PORT}",
+    ) as g3:
+        serial = await g3.system.get_recording_unit_serial()
+        assert type(serial) is str    
 
+async def main():
+    await test_connect_with_urls(G3_HOSTNAME)
 
 asyncio.run(main())
-"""
-['APIComponent', 'Any', 'AsyncIterator', 'Calibrate', 'Coroutine', 'DEFAULT_HTTP_PORT', 'DEFAULT_RTSP_LIVE_PATH',
- 'DEFAULT_RTSP_PORT', 'DEFAULT_WEBSOCKET_PATH', 'FeatureNotAvailableError', 'G3Service', 'G3ServiceDiscovery',
- 'G3WebSocketClientProtocol', 'Generator', 'Glasses3', 'LoggerLike', 'Optional', 'Recorder', 'Recordings', 'Rudimentary',
- 'Settings', 'Streams', 'System', 'TracebackType', 'Tuple', 'Type', 'URI', '__builtins__', '__cached__', '__doc__', '__file__',
- '__loader__', '__name__', '__package__', '__path__', '__spec__', '__version__', '_logger', '_utils', 'annotations',
- 'asynccontextmanager', 'calibrate', 'cast', 'connect_to_glasses', 'exceptions', 'g3pylib', 'g3typing', 'logging', 'recorder',
- 'recordings', 'rudimentary', 'settings', 'streams', 'system', 'websocket', 'zeroconf']"""
